bb00bf8e5f1202337e184e64a74b62e6244133ee
abreham nedi <abrehamnedi77@gmail.com>
2022-03-18 22:58:00 -0400

    Abreham

---
+++ CODE/First1/src/practice/student/Proffaser.java
@@ -0,0 +1,16 @@
+package practice.student;
+
+public class Proffaser {
+
+	private static int proffaserId = 8910;
+	private static String fullName = "bekele gerba";
+	private static double gpa = 1500.75;
+	
+public static void main (String[]args){
+	
+	System.out.println(proffaserId);
+	System.out.println(fullName);
+	System.out.println(gpa);
+	
+}
+}
---
+++ CODE/First1/src/practice/student/Student.java
@@ -0,0 +1,13 @@
+package practice.student;
+
+public class Student {
+	private static int studentId = 123;
+	private static String fullName = "Abebe Damitw";
+	private static double gpa = 3.6;		
+public static void main(String[] args) {
+  
+	System.out.println(studentId);
+	System.out.println(fullName);
+	System.out.println(gpa);
+   }
+}
