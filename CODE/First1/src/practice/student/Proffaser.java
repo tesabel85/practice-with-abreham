dc019f54d705ee611cb43cdfdb0ec38aa612e373
abreham nedi <abrehamnedi77@gmail.com>
2022-03-18 23:03:08 -0400

    prof

--- CODE/First1/src/practice/student/Proffaser.java
+++ CODE/First1/src/practice/student/Proffaser.java
@@ -4,13 +4,13 @@ public class Proffaser {
 
 	private static int proffaserId = 8910;
 	private static String fullName = "bekele gerba";
-	private static double gpa = 1500.75;
+	private static double salery = 1500.75;
 	
 public static void main (String[]args){
 	
 	System.out.println(proffaserId);
 	System.out.println(fullName);
-	System.out.println(gpa);
+	System.out.println(salery);
 	
 }
 }
