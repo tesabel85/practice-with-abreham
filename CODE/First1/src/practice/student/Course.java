946822f0fcf04b94627e250ddf151bee3ac536fa
abreham nedi <abrehamnedi77@gmail.com>
2022-03-20 17:50:00 -0400

    try

--- CODE/First1/src/practice/student/Course.java
+++ CODE/First1/src/practice/student/Course.java
@@ -5,10 +5,14 @@ public class Course {
 	private static int courseId = 1234;
 	private static String courseName = "biology";
 	private static double courseSalery = 100.78;
+	private static char midianIntial = 'k';
+	private static boolean isExtentionCourse = true;
   public static void main(String[]args) {
 	  System.out.println(courseId);
 	  System.out.println(courseName);
 	  System.out.println(courseSalery);
+	  System.out.println(isExtentionCourse);
+	  System.out.println(midianIntial);
   }
 }  
 
--- CODE/First1/src/practice/student/Student.java
+++ CODE/First1/src/practice/student/Student.java
@@ -3,11 +3,13 @@ package practice.student;
 public class Student {
 	private static int studentId = 123;
 	private static String fullName = "Abebe Damitw";
-	private static double gpa = 3.6;		
+	private static double gpa = 3.6;
+	private static char midianIntial = 'L';
 public static void main(String[] args) {
   
 	System.out.println(studentId);
 	System.out.println(fullName);
 	System.out.println(gpa);
+	System.out.println(midianIntial);
    }
 }
