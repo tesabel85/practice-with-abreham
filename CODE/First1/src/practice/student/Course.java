eb1d2e00376e5d9eb8b0a92452adf3d7b87724c9
abreham nedi <abrehamnedi77@gmail.com>
2022-03-20 17:11:52 -0400

    new course

---
+++ CODE/First1/src/practice/student/Course.java
@@ -0,0 +1,14 @@
+package practice.student;
+
+public class Course {
+	
+	private static int courseId = 1234;
+	private static String courseName = "biology";
+	private static double courseSalery = 100.78;
+  public static void main(String[]args) {
+	  System.out.println(courseId);
+	  System.out.println(courseName);
+	  System.out.println(courseSalery);
+  }
+}  
+
