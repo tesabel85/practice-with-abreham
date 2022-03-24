e63ed14cf6f2a5051e778bd53a7d2fbea888b7e2
abreham nedi <abrehamnedi77@gmail.com>
2022-03-24 18:13:14 -0400

    this is update file

--- CODE/First1/src/practice/student/GsustateExample.java
+++ CODE/First1/src/practice/student/GsustateExample.java
@@ -18,12 +18,72 @@ public static void main(String[]args) {
   System.out.println(y);
   System.out.println(x);
   String txt = "ABCDEFG";
-  System.out.println("The length of the txt string is: " + txt.length());
+  System.out.println("how many is leagth of txt string got: " + txt.length());
 System.out.println(userName);	
 System.out.println(userId);
 System.out.println(middleInitail);
 System.out.println(salary);
 System.out.println(isTheWorkHard);
+// all over Arithmetic Operators under this 
+int k = 5 + 6;
+System.out.println(k);
+int X = 6;
+int Y =3;
+System.out.println(X - Y);
+int D = 5;
+int F = 2;
+System.out.println(D % F);
+int T = 2;
+int U = 6;
+System.out.println(T * U);
+int G = 10;
+int R = 2;
+System.out.println(G / R);
+int C = 8;
+++C;
+System.out.println(C);
+int H = 7;
+--H;
+System.out.println(H);
+// A list of all assignment operators:
+int b = 6;
+ b -= 3;
+System.out.println(b);
+int v = 8;
+v += 2;
+System.out.println(v);
+int o = 5;
+o *= 2;
+System.out.println(o);
+double s = 3;
+s /= 2;
+System.out.println(s);
+int l = 6;
+l %= 3;
+System.out.println(l);
+// java Comparison Operators
+int r = 5;
+int t = 7;
+System.out.println(r != t);
+int m = 8;
+int n = 8;
+System.out.println(m == n);
+int e = 6;
+int p = 7;
+System.out.println(e > p);
+int j = 9;
+int w = 8;
+System.out.println(j < w);
+int i = 8;
+int u = 7;
+System.out.println(i >= u);
+int q = 100;
+int V = 71;
+System.out.println(q <= v);
 
-} 
+
+
+
+
+ } 
 }
