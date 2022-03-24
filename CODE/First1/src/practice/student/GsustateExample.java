9e6787fbee4166b7717399cf7125913526ab2427
abreham nedi <abrehamnedi77@gmail.com>
2022-03-23 20:18:03 -0400

    this my new code2

---
+++ CODE/First1/src/practice/student/GsustateExample.java
@@ -0,0 +1,29 @@
+package practice.student;
+
+public class GsustateExample {
+ private static String userName="Abebe";
+private static int userId = 547;
+private static char middleInitail = 't';
+private static double salary = 20.95;
+private static boolean isTheWorkHard = true;
+public static void main(String[]args) {
+ double myDouble = 20.95;
+ int myInt = (int)myDouble;
+ System.out.println(myDouble);
+ System.out.println(myInt);
+ int x = 5;
+  ++x;
+  int y = 2;
+  y *= 4;
+  System.out.println(y);
+  System.out.println(x);
+  String txt = "ABCDEFG";
+  System.out.println("The length of the txt string is: " + txt.length());
+System.out.println(userName);	
+System.out.println(userId);
+System.out.println(middleInitail);
+System.out.println(salary);
+System.out.println(isTheWorkHard);
+
+} 
+}
